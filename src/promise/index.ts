293ef07b1e238b2eaad93e8ef5258657c9edfcad
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-07-28 23:36:55 -0300

    Clase N°10: Qué son las promesas.

---
+++ src/promise/index.ts
@@ -0,0 +1,15 @@
+const promise = new Promise((resolve, reject) => {
+    resolve('Hey!');
+});
+
+const cows = 9;
+const countCows = new Promise((resolve, reject) => {
+    cows > 10
+        ? resolve(`We have ${cows} cows on the farm.`)
+        : reject('There is no cows on the farm.');
+});
+
+countCows
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+    .finally(() => console.log('Finaly.'));
