fab98bb8517088ad9733c333a375c081cdb3577b
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-08-01 20:45:02 -0300

    Clase N°11: Fetch.

---
+++ src/promise/challenge.ts
@@ -0,0 +1,30 @@
+(() => {
+    const API = 'https://api.escuelajs.co/api/v1';
+
+    const fetchData = (url: string) => {
+        return fetch(url);
+    };
+
+    // fetchData(`${API}/products`)
+    //     .then(response => response.json())
+    //     .then(products => console.log(products))
+    //     .then(() => console.log('Hello!'))
+    //     .catch(error => console.error(error));
+
+    fetchData(`${API}/products`)
+        .then(response => response.json())
+        .then(products => {
+            // console.log(products);
+            return fetchData(`${API}/products/${products[0].id}`);
+        })
+        .then(response => response.json())
+        .then(product => {
+            console.log(product.title);
+            return fetchData(`${API}/categories/${product.category.id}`);
+        })
+        .then(response => response.json())
+        .then(category => console.log(category))
+        .catch(error => console.error(error))
+        .finally(() => console.log('Finally'));
+})();
+
